a44d3593ffe43f73e3460bd0bac9bb0aa36806e6
Candela94 <candelafsg@alumnos.cei.es>
2025-03-01 10:16:03 +0100

    bottom-nav

---
+++ app-music/final-project/src/Layout.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './css/Layout.css'
+import { BottomNavigation } from './components/bottom-navigation-header/BottomNavigation'
+
+
+
+
+
+
+function Layout() {
+  
+
+  return (
+    <>
+    <BottomNavigation />
+    </>
+  )
+}
+
+export default Layout
